420313718e9f451b24c3c6585e4c84097e423ea8
Christian Manzaraz <magnusmanz@gmail.com>
2024-01-23 21:35:41 -0300

    Initial Commit

---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Christian Manzaraz on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
